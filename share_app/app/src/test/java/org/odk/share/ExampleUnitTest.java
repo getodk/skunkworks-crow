dac62111e9365049f48132a76dd861b36a6d3daa
lakshyagupta21 <lakshyagupta21@gmail.com>
2018-05-15 12:07:27 +0530

    Code quality checks added

--- share_app/app/src/test/java/org/odk/share/ExampleUnitTest.java
+++ share_app/app/src/test/java/org/odk/share/ExampleUnitTest.java
@@ -2,7 +2,8 @@ package org.odk.share;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
 
 /**
  * Example local unit test, which will execute on the development machine (host).
