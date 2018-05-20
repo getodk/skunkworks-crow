c97da80139d40f65b2f540b50a0d5a07a363fd03
lakshyagupta21 <lakshyagupta21@gmail.com>
2018-05-20 16:38:09 +0530

    Files from collect added

---
+++ share_app/src/main/java/org/odk/share/utilities/ApplicationConstants.java
@@ -0,0 +1,15 @@
+package org.odk.share.utilities;
+
+/**
+ * Created by laksh on 5/20/2018.
+ */
+
+public class ApplicationConstants {
+
+    // based on http://www.sqlite.org/limits.html
+    public static final int SQLITE_MAX_VARIABLE_NUMBER = 999;
+
+    private ApplicationConstants() {
+
+    }
+}
