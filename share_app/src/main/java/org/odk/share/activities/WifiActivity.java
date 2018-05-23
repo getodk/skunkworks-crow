fe545b586e44d71e95e83f093c522ef15e4a10b3
lakshyagupta21 <lakshyagupta21@gmail.com>
2018-05-23 17:38:28 +0530

    dialog added for password protected networks

--- share_app/src/main/java/org/odk/share/activities/WifiActivity.java
+++ share_app/src/main/java/org/odk/share/activities/WifiActivity.java
@@ -2,18 +2,27 @@ package org.odk.share.activities;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.InputType;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -37,8 +46,10 @@ public class WifiActivity extends AppCompatActivity {
     TextView emptyView;
 
     private WifiManager wifiManager;
-    WifiResultAdapter wifiResultAdapter;
-    List<ScanResult> scanResultList;
+    private WifiResultAdapter wifiResultAdapter;
+    private List<ScanResult> scanResultList;
+    private boolean isReceiverRegistered;
+    private AlertDialog alertDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,9 +81,16 @@ public class WifiActivity extends AppCompatActivity {
     @Override
     protected void onStop() {
         super.onStop();
+
+        if (alertDialog != null && alertDialog.isShowing()) {
+            alertDialog.dismiss();
+        }
+
         try {
-            unregisterReceiver(receiver);
             unregisterReceiver(wifiStateReceiver);
+            if (isReceiverRegistered) {
+                unregisterReceiver(receiver);
+            }
         } catch (IllegalArgumentException e) {
             Timber.e(e);
         }
@@ -80,12 +98,71 @@ public class WifiActivity extends AppCompatActivity {
 
     private void onListItemClick(View view, int i) {
         Timber.d("Clicked " + scanResultList.get(i));
+        if (Wifi.isClose(scanResultList.get(i))) {
+            // Show dialog and ask for password
+            showPasswordDialog(scanResultList.get(i));
+        } else {
+            // connect
+        }
+    }
+
+    private void showPasswordDialog(ScanResult scanResult) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        LayoutInflater factory = LayoutInflater.from(this);
+        final View dialogView = factory.inflate(R.layout.dialog_password, null);
+        final EditText passwordEditText = dialogView.findViewById(R.id.etPassword);
+        final CheckBox passwordCheckBox = dialogView.findViewById(R.id.checkBox);
+        passwordCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (!passwordCheckBox.isChecked()) {
+                    passwordEditText.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                } else {
+                    passwordEditText.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                }
+            }
+        });
+        builder.setTitle(scanResult.SSID);
+        builder.setView(dialogView);
+        builder.setPositiveButton(getString(R.string.connect), null);
+        builder.setNegativeButton(getString(R.string.cancel), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        builder.setCancelable(false);
+        alertDialog = builder.create();
+        alertDialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+        alertDialog.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface dialog) {
+                Button button = ((AlertDialog) dialog).getButton(AlertDialog.BUTTON_POSITIVE);
+                button.setOnClickListener(new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View view) {
+                        String pw = passwordEditText.getText().toString();
+                        if (!pw.equals("")) {
+                            Timber.d(pw);
+                            dialog.dismiss();
+                        } else {
+                            passwordEditText.setError(getString(R.string.password_empty));
+                        }
+                    }
+                });
+            }
+        });
+        alertDialog.show();
     }
 
     public void startScan() {
         scanResultList.clear();
         wifiResultAdapter.notifyDataSetChanged();
         setEmptyViewVisibility(getString(R.string.scanning));
+        isReceiverRegistered = true;
         registerReceiver(receiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
         wifiManager.startScan();
     }
@@ -94,6 +171,7 @@ public class WifiActivity extends AppCompatActivity {
         @Override
         public void onReceive(Context c, Intent intent) {
             scanResultList.addAll(wifiManager.getScanResults());
+            isReceiverRegistered = false;
             unregisterReceiver(this);
             wifiResultAdapter.notifyDataSetChanged();
             setEmptyViewVisibility(getString(R.string.no_wifi_available));
