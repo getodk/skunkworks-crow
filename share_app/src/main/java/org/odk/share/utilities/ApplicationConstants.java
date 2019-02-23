ff81afa7680ef5a6f768727a21276367da160474
Shobhit Agarwal <shobhitagarwal1612@gmail.com>
2019-02-23 19:42:33 +0530

    Use adapter's context instead of Share.getInstance()

--- share_app/src/main/java/org/odk/share/utilities/ApplicationConstants.java
+++ share_app/src/main/java/org/odk/share/utilities/ApplicationConstants.java
@@ -1,7 +1,8 @@
 package org.odk.share.utilities;
 
+import android.content.Context;
+
 import org.odk.share.R;
-import org.odk.share.application.Share;
 
 import java.util.HashMap;
 
@@ -18,23 +19,23 @@ public class ApplicationConstants {
     public static final int SEND_BLANK_FORM_MODE = 3;
     public static final int SEND_FILL_FORM_MODE = 4;
 
+    private ApplicationConstants() {
+
+    }
+
+    public static HashMap<String, Integer> getSortLabelToIconMap(Context context) {
+        HashMap<String, Integer> hashMap = new HashMap<>();
+        hashMap.put(context.getString(R.string.sort_by_name_asc), R.drawable.ic_sort_by_alpha);
+        hashMap.put(context.getString(R.string.sort_by_name_desc), R.drawable.ic_sort_by_alpha);
+        hashMap.put(context.getString(R.string.sort_by_date_asc), R.drawable.ic_access_time);
+        hashMap.put(context.getString(R.string.sort_by_date_desc), R.drawable.ic_access_time);
+        return hashMap;
+    }
+
     public abstract static class SortingOrder {
         public static final int BY_NAME_ASC = 0;
         public static final int BY_NAME_DESC = 1;
         public static final int BY_DATE_DESC = 2;
         public static final int BY_DATE_ASC = 3;
-    }
-
-    public static HashMap<String, Integer> getSortLabelToIconMap() {
-        HashMap<String, Integer> hashMap = new HashMap<>();
-        hashMap.put(Share.getInstance().getString(R.string.sort_by_name_asc), R.drawable.ic_sort_by_alpha);
-        hashMap.put(Share.getInstance().getString(R.string.sort_by_name_desc), R.drawable.ic_sort_by_alpha);
-        hashMap.put(Share.getInstance().getString(R.string.sort_by_date_asc), R.drawable.ic_access_time);
-        hashMap.put(Share.getInstance().getString(R.string.sort_by_date_desc), R.drawable.ic_access_time);
-        return hashMap;
-    }
-
-    private ApplicationConstants() {
-
     }
 }
