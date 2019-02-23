68ca7bd29c6bcd3d0c63b769197e698dd5fe336d
Shobhit Agarwal <shobhitagarwal1612@gmail.com>
2019-02-23 20:14:27 +0530

    Add base class InjectableFragment

---
+++ share_app/src/main/java/org/odk/share/fragments/InjectableFragment.java
@@ -0,0 +1,12 @@
+package org.odk.share.fragments;
+
+import dagger.android.support.DaggerFragment;
+
+/**
+ * Dependencies of the activity extending {@link InjectableFragment} which are annotated with
+ * {@link javax.inject.Inject} will be automatically injected.
+ * <p>
+ * Those activities should also be registered in {@link org.odk.share.injection.FragmentBuilder}
+ */
+public class InjectableFragment extends DaggerFragment {
+}
