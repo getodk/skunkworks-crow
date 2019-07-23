1611fbba59720af21f13763b377293cf54773f87
huangyz0918 <huangyz0918@gmail.com>
2019-07-23 11:54:47 +0800

    🚑 quickfix: removed the unreachable bluetooth devices.

--- skunkworks_crow/src/main/java/org/odk/share/views/ui/bluetooth/BluetoothListAdapter.java
+++ skunkworks_crow/src/main/java/org/odk/share/views/ui/bluetooth/BluetoothListAdapter.java
@@ -21,6 +21,7 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 import static android.bluetooth.BluetoothDevice.BOND_BONDED;
+import static android.bluetooth.BluetoothDevice.BOND_NONE;
 
 
 /**
@@ -69,7 +70,7 @@ public class BluetoothListAdapter extends RecyclerView.Adapter<BluetoothListAdap
         int deviceBondState = device.getBondState();
         viewHolder.deviceName.setText(deviceName == null ? viewHolder.itemView.getResources().getString(R.string.bluetooth_instance_name_default) : deviceName);
         viewHolder.deviceAddress.setText(String.format("%s (%s)", deviceAddress,
-                deviceBondState == 10 ? context.getString(R.string.bluetooth_unpaired) : context.getString(R.string.bluetooth_paired)));
+                deviceBondState == BOND_NONE ? context.getString(R.string.bluetooth_unpaired) : context.getString(R.string.bluetooth_paired)));
         if (device.getBondState() == BOND_BONDED) {
             viewHolder.deviceLogo.setImageResource(R.drawable.ic_smart_phone_yellow);
         }
