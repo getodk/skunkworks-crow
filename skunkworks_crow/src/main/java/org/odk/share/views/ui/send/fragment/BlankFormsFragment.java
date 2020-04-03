63411b1b6c0fbeaac60f79a4d5d2edf357924631
SaumiaSinghal <saumiasinghal@gmail.com>
2020-04-03 17:23:08 +0530

    fixed pmd

--- skunkworks_crow/src/main/java/org/odk/share/views/ui/send/fragment/BlankFormsFragment.java
+++ skunkworks_crow/src/main/java/org/odk/share/views/ui/send/fragment/BlankFormsFragment.java
@@ -2,13 +2,9 @@
 
 package org.odk.share.views.ui.send.fragment;
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.database.Cursor;
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,7 +13,6 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.core.app.ActivityCompat;
 import androidx.loader.app.LoaderManager;
 import androidx.loader.content.Loader;
 import androidx.recyclerview.widget.DividerItemDecoration;
